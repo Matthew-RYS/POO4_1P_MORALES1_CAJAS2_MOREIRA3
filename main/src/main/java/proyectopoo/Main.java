c7a5a95c21d6e48c1d899e35f05ee63b5144fd2a
Matthew Morales <matemora@espol.edu.ec>
2024-11-09 16:28:48 -0500

    cambio de hola mundo a hello world

--- main/src/main/java/proyectopoo/Main.java
+++ main/src/main/java/proyectopoo/Main.java
@@ -2,6 +2,6 @@ package proyectopoo;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hola mundo!");
+        System.out.println("Hello World!");
     }
 }
