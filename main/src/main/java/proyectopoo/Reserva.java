e29d05ad69ca070d309c55698fa3052dba420ddf
Matthew Morales <matemora@espol.edu.ec>
2024-11-10 17:38:38 -0500

    Creación de las variables

---
+++ main/src/main/java/proyectopoo/Reserva.java
@@ -0,0 +1,13 @@
+package proyectopoo;
+
+public class Reserva {
+    public String Feccha;
+    //public Espacio espacioDisponible;
+    public char confirmacion;
+    public Usuario usuario;
+    public String estado;
+    public int cantidadReserva;
+    public int codigoReserva;
+    public String motivo;
+
+}
