a64b366e10190991e392f23df12dd6cd93634a45
Matthew Morales <matemora@espol.edu.ec>
2024-11-25 20:30:14 -0500

    Creacion de constructor de inicializar la clase Admin que llama al constructor de la clase Usuario

--- main/src/main/java/proyectopoo/Admin.java
+++ main/src/main/java/proyectopoo/Admin.java
@@ -3,6 +3,12 @@ import java.util.Date;
 
 public class Admin extends Usuario{
     private String cargo;
+
+    public Admin(String codigoUnico, String cedula, String nombres, String apellidos, String ususario, String contrasena, String correo, String rol, String cargo){
+        super(codigoUnico, cedula, nombres, apellidos, ususario, contrasena, correo, rol);
+        this.cargo=cargo;
+    }
+
     @Override
     public void gestionarReserva(Date fecha, Espacio espacio, String motivo){
     }
