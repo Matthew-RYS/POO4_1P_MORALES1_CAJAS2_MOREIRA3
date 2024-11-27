e8f59732374b05f51857505675420981da75771f
Misael <mijomore@espol.edu.ec>
2024-11-27 13:31:40 -0500

    Comentario

--- main/src/main/java/proyectopoo/Estudiante.java
+++ main/src/main/java/proyectopoo/Estudiante.java
@@ -1,12 +1,10 @@
 package proyectopoo;
-import java.util.ArrayList;
 import java.util.Date;
 
 
 public class Estudiante extends Usuario{
     private String matricula;
     private String carrera;
-    protected ArrayList<Reserva> reservasRealizadas;
 
     public Estudiante(String codigoUnico, String cedula, String nombres, String apellidos, String ususario, String contrasena, String correo, Rol rol, String matricula, String carrera){
         super(codigoUnico, cedula, nombres, apellidos, ususario, contrasena, correo, rol);
@@ -20,7 +18,9 @@ public class Estudiante extends Usuario{
         Reserva r = new Reserva(fecha, codigo, motivo, Estado.PENDIENTE,this.codigoUnico);
         r.enviarCorreo();
         Sistema.reservas.add(r);
-        return r;
+        return r; //Aqui falta implementarlo bien en el menu de estudiantes, en el pdf primero se muestra el listado de espacios disponibles
+                  //dependiendo de cual escoja, si realiza o no la reserva esta se guarda y se envia el correo, por ello primero se deberia hacer la validacion
+                  //y luego usar el metodo
     }
     @Override
     public void consultarReserva(Date fecha){
